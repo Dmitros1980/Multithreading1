f09f7a0e772cb7beb30c8253b7146d78a6257c1b
Dmitros <dimayurkin5@mail.ru>
2023-06-20 00:43:47 +0400

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.util.*;
 
 
 public class Main {
+
     public static void main(String[] args) throws InterruptedException {
         List<Thread> threads = new ArrayList<>();
         Runnable runnable = () -> {
