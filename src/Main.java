7327b92597965c2e0824e5e1aff5fd5ee23db9bb
Dmitros <dimayurkin5@mail.ru>
2023-06-29 17:52:56 +0400

    commit 2

--- src/Main.java
+++ src/Main.java
@@ -6,88 +6,22 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException {
         List<Thread> threads = new ArrayList<>();
-        Runnable runnable = () -> {
-            String[] texts = new String[5];
-            for (int i = 0; i < texts.length; i++) {
-                texts[i] = generateText("aab", 30_000);
-            }
-            long startTs = System.currentTimeMillis(); // start time
-            for (String text : texts) {
-                int maxSize = 0;
-                for (int i = 0; i < text.length(); i++) {
-                    for (int j = 0; j < text.length(); j++) {
-                        if (i >= j) {
-                            continue;
-                        }
-                        boolean bFound = false;
-                        for (int k = i; k < j; k++) {
-                            if (text.charAt(k) == 'b') {
-                                bFound = true;
-                                break;
-                            }
-                        }
-                        if (!bFound && maxSize < j - i) {
-                            maxSize = j - i;
-                        }
-                    }
-                }
-                System.out.println(text.substring(0, 100) + " -> " + maxSize);
-                long endTs = System.currentTimeMillis(); // end time
-
-                System.out.println("Time: " + (endTs - startTs) + "ms");
-
-            }
-            try {
-                Thread.sleep(200);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        };
-        Thread thread = new Thread(runnable);
-        Thread thread1 = new Thread(runnable);
-        Thread thread2 = new Thread(runnable);
-        Thread thread3 = new Thread(runnable);
-        thread.start();
-        thread1.start();
-        thread2.start();
-        thread3.start();
-        for (Thread threades : threads) {
-            try {
-                thread.join(); // зависаем, ждём когда поток объект которого лежит в thread завершится
-                thread1.join();
-                thread2.join();
-                thread3.join();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-
-        String[] texts = new String[5];
+        String[] texts = new String[25];
         for (int i = 0; i < texts.length; i++) {
             texts[i] = generateText("aab", 30_000);
         }
-
         long startTs = System.currentTimeMillis(); // start time
+
         for (String text : texts) {
-            int maxSize = 0;
-            for (int i = 0; i < text.length(); i++) {
-                for (int j = 0; j < text.length(); j++) {
-                    if (i >= j) {
-                        continue;
-                    }
-                    boolean bFound = false;
-                    for (int k = i; k < j; k++) {
-                        if (text.charAt(k) == 'b') {
-                            bFound = true;
-                            break;
-                        }
-                    }
-                    if (!bFound && maxSize < j - i) {
-                        maxSize = j - i;
-                    }
-                }
-            }
-            System.out.println(text.substring(0, 100) + " -> " + maxSize);
+            threads.add(getNewThread(text));
+        }
+
+        for (Thread thread : threads) {
+            thread.start();
+        }
+
+        for (Thread thread : threads) {
+            thread.join();
         }
         long endTs = System.currentTimeMillis(); // end time
 
@@ -103,5 +37,31 @@ public class Main {
             text.append(letters.charAt(random.nextInt(letters.length())));
         }
         return text.toString();
+    }
+
+    public static Thread getNewThread(String text) {
+        return new Thread(
+                () -> {
+                    int maxSize = 0;
+                    for (int i = 0; i < text.length(); i++) {
+                        for (int j = 0; j < text.length(); j++) {
+                            if (i >= j) {
+                                continue;
+                            }
+                            boolean bFound = false;
+                            for (int k = i; k < j; k++) {
+                                if (text.charAt(k) == 'b') {
+                                    bFound = true;
+                                    break;
+                                }
+                            }
+                            if (!bFound && maxSize < j - i) {
+                                maxSize = j - i;
+                            }
+                        }
+                    }
+                    System.out.println(text.substring(0, 100) + " -> " + maxSize);
+                }
+        );
     }
 }
